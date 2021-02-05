c2ed88a9a31c956d9ae99a7116861fd98b6a0616
Dhairya <dhairya.ostwal@gmail.com>
2021-02-05 10:06:17 +0530

    Float explanation

--- first.java
+++ first.java
@@ -4,6 +4,8 @@ public static void main(String ar[])
 {
    // display statement - to display something on the screen
    System.out.println("Hello! My first Java Class");
+   float someNum = 23.45f;
+   System.out.println(someNum);
 }
 }
 // package com.company;
