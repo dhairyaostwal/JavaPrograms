661782104f61bfe41634d87fdc7109036571c4f1
Dhairya <dhairya.ostwal@gmail.com>
2021-02-10 08:47:39 +0530

    Static Class Program

--- Theory-Programs/staticmain.java
+++ Theory-Programs/staticmain.java
@@ -1,6 +1,6 @@
 class static1{
     int a=100;
-    static int b=300;
+    static int b=300; // use of static prevents object creation
 }
 
 class static2{
@@ -9,9 +9,9 @@ class static2{
 
 class staticmain1{
     public static void main(String a[]){
-        static1 st =new static1();
+        static1 st =new static1(); // object created
         System.out.println(st.a);
-        System.out.println(static1.b);
+        System.out.println(static1.b); 
         System.out.println(static2.b);
     }
 }
